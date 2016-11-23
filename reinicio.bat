f816af911a7bc5fc86a30bf3c0e120a4152edf02
Luis Enrique Canseco Gutiérrez <lcanseco.enrique@gmail.com>
2016-11-22 22:02:46 -0600

    añadir archivos bat
    Agregar los archivos bat que reinician y apagan el equipo, limpian los
    temporales y algunas otras funciones.

---
+++ reinicio.bat
@@ -0,0 +1,35 @@
+@echo off
+
+rem subir directorio hasta C
+cd C:\
+
+rem terminar programas con estatus no responda
+taskkill.exe /f /fi "status eq Not Responding"
+
+rem limpiar los dns
+ipconfig /flushdns
+
+ipconfig /renew *
+netsh int ip set dns
+
+netsh winsock reset
+
+ipconfig /release *
+
+rem borrar temporales
+del *.tmp /s /f
+
+
+rem ARCHIVOS TEMPORALES DE INTERNET
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+rem COOKIES 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 2 
+rem HISTORIAL
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 1 
+rem FORMULARIOS 
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 16
+
+REM reiniciado
+shutdown /r
+
+rem apagado shutdown /s
